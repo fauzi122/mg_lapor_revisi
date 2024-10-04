8c1f6fabe8ccf016ec7d0e15309f67ff4ba5a29a
fauzi ahmad <ozanfauzi39@gmail.com>
2024-10-04 17:44:49 +0700

    Your commit message here

--- resources/views/badan_usaha/pengolahan/minyak_bumi/index.blade.php
+++ resources/views/badan_usaha/pengolahan/minyak_bumi/index.blade.php
@@ -9,7 +9,7 @@
                         <div class="page-title-right">
                             <ol class="breadcrumb m-0">
                                 <li class="breadcrumb-item"><a href="javascript: void(0);">Pengolahan</a></li>
-                                <li class="breadcrumb-item active"> Minyak Bumi/Hasil Olahan< & Gas Bumi/li>
+                                <li class="breadcrumb-item active"> Minyak Bumi/Hasil Olahan< & Gas Bumi</li>
                             </ol>
                         </div>
                     </div>
