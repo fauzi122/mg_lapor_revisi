30fcb0a697f77af04b7a26812fd217d863b3b87e
fauzi ahmad <ozanfauzi39@gmail.com>
2024-11-20 18:43:17 +0700

    evaluator

--- resources/views/evaluator/laporan_bu/hasil_olah_mb/jual_hasil/index.blade.php
+++ resources/views/evaluator/laporan_bu/hasil_olah_mb/jual_hasil/index.blade.php
@@ -112,6 +112,8 @@
                                                 <tr>
                                                     <th>No</th>
                                                     <th>Nama Perusahaan</th>
+                                                    <th>Nomer Izin</th>
+                                                    <th>Tanggal Disetujui</th>
                                                     <th>Aksi</th>
                                                 </tr>
                                             </thead>
@@ -120,6 +122,8 @@
                                                     <tr>
                                                         <td>{{ $loop->iteration }}</td>
                                                         <td>{{ $per->NAMA_PERUSAHAAN }}</td>
+                                                        <td>{{ $per->TGL_DISETUJUI }}</td>
+                                                        <td>{{ $per->NOMOR_IZIN }}</td>
                                                         <td>
                                                             <a href="{{ url('laporan/jual-hasil-olahan/periode') . '/' . \Illuminate\Support\Facades\Crypt::encrypt($per->id_perusahaan) }}"
                                                                 class="btn btn-primary btn-rounded btn-sm"><i
