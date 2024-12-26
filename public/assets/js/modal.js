bc51d23318867f449318e2ea7c4e234c08f17d96
fauzi ahmad <ozanfauzi39@gmail.com>
2024-12-26 20:53:20 +0700

    Merubah js aset

--- public/assets/js/modal.js
+++ public/assets/js/modal.js
@@ -1,8 +1,8 @@
 
-// Tentukan apakah situs diakses dari localhost atau lapor.duniasakha.com
-var isLocalhost = window.location.hostname === '127.0.0.1' || window.location.hostname === 'localhost' || window.location.hostname === 'lapor.duniasakha.com';
+// Tentukan apakah situs diakses dari localhost atau duniasakha.com
+var isLocalhost = window.location.hostname === '127.0.0.1' || window.location.hostname === 'localhost' || window.location.hostname === 'duniasakha.com';
 
-// Atur baseUrl berdasarkan apakah situs diakses dari localhost atau lapor.duniasakha.com
+// Atur baseUrl berdasarkan apakah situs diakses dari localhost atau duniasakha.com
 var baseUrl = isLocalhost ? "/" : "/pelaporan-hilir/";
 
 function editPenjualan(id, produk, kabupaten_kota) {
