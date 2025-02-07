93c6545f8a14bd0c8fac27a9fbff6ad6e53fb9d3
vanturgo16 <vanturgo16@gmail.com>
2025-02-07 10:38:03 +0700

    test

--- app/Http/Controllers/auth/AuthEvaluatorController.php
+++ app/Http/Controllers/auth/AuthEvaluatorController.php
@@ -154,6 +154,7 @@ class AuthEvaluatorController extends Controller
 		
         if ($request->has('ticket')) {
 			list($verified, $data, $error) = $this->verifySSOTicket($request->get('ticket'));
+			dd($data);
 			$user = User::where('email', $email)->first();
 			
 			$sessions = Session::whereNull('user_id')->where('user_id', $user->id)->get();
