c144f7314b58510725755b6367d2c83f301209db
Hardy Prabowo <hardy.prabowo@napinfo.co.id>
2025-06-09 17:04:07 +0700

    done content for service inline, belum yg link login

--- app/Http/Controllers/API/BadanUsahaController.php
+++ app/Http/Controllers/API/BadanUsahaController.php
@@ -7,6 +7,9 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\API\BaseController as BaseController;
 use Illuminate\Support\Facades\Validator;
 use App\Models\User;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\DB;
 
 class BadanUsahaController extends BaseController
 {
@@ -36,4 +39,234 @@ class BadanUsahaController extends BaseController
    
         return $this->sendResponse($success, 'Sukses Registrasi Badan Usaha.');
 	}
+
+    public function getDataLaporanBU(Request $request){
+        $npwp = $request->NPWP;
+        
+        //validasi jika npwp null
+        if ($npwp == 'null' || $npwp == "") {
+            $message = "NPWP tidak boleh kosong";
+            $response = [
+                'status' => "error",
+                'message' => $message,
+            ];
+            return response()->json($response, 200);
+        }
+
+        $message = "Data Pelaporan";
+        App::setLocale('id'); // Set locale ke bahasa Indonesia
+        Carbon::setLocale('id');
+        $eom = Carbon::now()->endOfMonth()->translatedFormat('d F Y');
+        $bulan = Carbon::now()->endOfMonth()->translatedFormat('Y-m');
+        $badanUsahaId = User::where('npwp', $npwp)->value('badan_usaha_id');
+
+        //Laporan NIAGA
+        $queriesNiaga = [
+            // MINYAK BUMI
+            ['table' => 'jual_hasil_olah_bbms', 'column' => 'bulan'],
+            ['table' => 'pasokan_hasil_olah_bbms', 'column' => 'bulan'],
+            ['table' => 'harga_bbm_jbus', 'column' => 'bulan'],
+            ['table' => 'ekspors', 'column' => 'bulan_peb'],
+            ['table' => 'impors', 'column' => 'bulan_pib'],
+            ['table' => 'penyminyakbumis', 'column' => 'bulan'],
+            
+            // GAS
+            ['table' => 'harga_l_p_g_s', 'column' => 'bulan'],
+            ['table' => 'penjualan_lngs', 'column' => 'bulan'],
+            ['table' => 'pasokanlngs', 'column' => 'bulan'],
+            ['table' => 'penjualan_lpgs', 'column' => 'bulan'],
+            ['table' => 'pasokan_l_p_g_s', 'column' => 'bulan'],
+            ['table' => 'ekspors', 'column' => 'bulan_peb'],
+            ['table' => 'impors', 'column' => 'bulan_pib'],
+        ];
+
+        $foundNiaga = false;
+
+        foreach ($queriesNiaga as $queryNiaga) {
+            $resultNiaga = DB::table("setum_kop.{$queryNiaga['table']}")
+                ->where(DB::raw("DATE_FORMAT({$queryNiaga['column']}, '%Y-%m')"), $bulan)
+                ->where('badan_usaha_id', $badanUsahaId)
+                ->where('status', '<>', 1)
+                ->exists();
+
+            if ($resultNiaga) {
+                $foundNiaga = true;
+                break;
+            }
+            else{
+                $foundNiaga = false;
+                break;
+            }
+        }
+
+        if ($foundNiaga = true) {
+            $statusNiaga = 'Draft';
+        } else {
+            $statusNiaga = 'Diterima';
+        }
+        //Laporan NIAGA
+
+        //Laporan Pengolahan
+        $queriesPengolahan = [
+            // MINYAK BUMI
+            ['table' => 'harga_bbm_jbus', 'column' => 'bulan'],
+            ['table' => 'pengolahan_minyak_bumi_produksis', 'column' => 'bulan'],
+            ['table' => 'pengolahan_minyak_bumi_pasokans', 'column' => 'bulan'],
+            ['table' => 'pengolahan_minyak_bumi_distribusis', 'column' => 'bulan'],
+            ['table' => 'ekspors', 'column' => 'bulan_peb'],
+            ['table' => 'impors', 'column' => 'bulan_pib'],
+            ['table' => 'penyminyakbumis', 'column' => 'bulan'],
+
+            // GAS
+            ['table' => 'pengolahans', 'column' => 'bulan', 'extra' => ['jenis' => 'Gas Bumi']],
+        ];
+
+        $foundPengolahan = false;
+
+        foreach ($queriesPengolahan as $queryPengolahan) {
+            $resultPengolahan = DB::table("setum_kop.{$queryPengolahan['table']}")
+                ->where(DB::raw("DATE_FORMAT({$queryPengolahan['column']}, '%Y-%m')"), $bulan)
+                ->where('badan_usaha_id', $badanUsahaId)
+                ->where('status', '<>', 1);
+
+            // Tambahan filter jika ada 'extra'
+            if (isset($queryPengolahan['extra'])) {
+                foreach ($queryPengolahan['extra'] as $field => $value) {
+                    $resultPengolahan->where($field, $value);
+                }
+            }
+
+            if ($resultPengolahan->exists()) {
+                $foundPengolahan = true;
+                break;
+            }
+            else{
+                $foundPengolahan = false;
+                break;
+            }
+        }
+
+        if ($foundPengolahan = true) {
+            $statusPengolahan = 'Draft';
+        } else {
+            $statusPengolahan = 'Diterima';
+        }
+        //Laporan Pengolahan
+
+        //laporan Penyimpanan
+        $queriesPenyimpanan = [
+            ['table' => 'penyminyakbumis', 'column' => 'bulan'], // MINYAK BUMI
+            ['table' => 'penygasbumis', 'column' => 'bulan'],    // GAS BUMI
+        ];
+
+        $found = false;
+
+        foreach ($queriesPenyimpanan as $queryPenyimpanan) {
+            $resultPenyimpanan = DB::table("setum_kop.{$queryPenyimpanan['table']}")
+                ->where(DB::raw("DATE_FORMAT({$queryPenyimpanan['column']}, '%Y-%m')"), $bulan)
+                ->where('badan_usaha_id', $badanUsahaId)
+                ->where('status', '<>', 1);
+
+            if ($resultPenyimpanan->exists()) {
+                $foundPenyimpanan = true;
+                break;
+            }
+            else{
+                $foundPenyimpanan = false;
+                break;
+            }
+        }
+
+        if ($foundPenyimpanan = true) {
+            $statusPenyimpanan = 'Draft';
+        } else {
+            $statusPenyimpanan = 'Diterima';
+        }
+        //laporan Penyimpanan
+
+        //laporan Pengangkutan
+        $queriesPengangkutan = [
+            ['table' => 'pengangkutan_minyakbumis', 'column' => 'bulan'], // MINYAK BUMI
+            ['table' => 'pengangkutan_gaskbumis', 'column' => 'bulan'],    // GAS BUMI
+        ];
+
+        $found = false;
+
+        foreach ($queriesPengangkutan as $queryPengangkutan) {
+            $resultPengangkutan = DB::table("setum_kop.{$queryPengangkutan['table']}")
+                ->where(DB::raw("DATE_FORMAT({$queryPengangkutan['column']}, '%Y-%m')"), $bulan)
+                ->where('badan_usaha_id', $badanUsahaId)
+                ->where('status', '<>', 1);
+
+            if ($resultPengangkutan->exists()) {
+                $foundPengangkutan = true;
+                break;
+            }
+            else{
+                $foundPengangkutan = false;
+                break;
+            }
+        }
+
+        if ($foundPengangkutan = true) {
+            $statusPengangkutan = 'Draft';
+        } else {
+            $statusPengangkutan = 'Diterima';
+        }
+        //laporan Pengangkutan
+        
+        $dataContent = [
+            'laporan_rutin' => [
+                [
+                    'jenis_pelaporan' => 'Pelaporan Niaga Migas',
+                    'batas_akhir_pelaporan' => $eom,
+                    'url' => '',
+                ],
+                [
+                    'jenis_pelaporan' => 'Pelaporan Pengolahan Migas',
+                    'batas_akhir_pelaporan' => $eom,
+                    'url' => '',
+                ],
+                [
+                    'jenis_pelaporan' => 'Pelaporan Penyimpanan Migas',
+                    'batas_akhir_pelaporan' => $eom,
+                    'url' => '',
+                ],
+                [
+                    'jenis_pelaporan' => 'Pelaporan Pengangkutan Migas',
+                    'batas_akhir_pelaporan' => $eom,
+                    'url' => '',
+                ]
+            ],
+            'layanan_penunjang' => [],
+            'status_laporan_rutin' => [
+                [
+                    'jenis_layanan'         => 'Pelaporan Niaga Migas',
+                    'batas_akhir_pelaporan' => $eom,
+                    'status_laporan'        => $statusNiaga
+                ],
+                [
+                    'jenis_layanan'         => 'Pelaporan Pengolahan Migas',
+                    'batas_akhir_pelaporan' => $eom,
+                    'status_laporan'        => $statusPengolahan
+                ],
+                [
+                    'jenis_layanan'         => 'Pelaporan Penyimpanan Migas',
+                    'batas_akhir_pelaporan' => $eom,
+                    'status_laporan'        => $statusPenyimpanan
+                ],
+                [
+                    'jenis_layanan'         => 'Pelaporan Pengangkutan Migas',
+                    'batas_akhir_pelaporan' => $eom,
+                    'status_laporan'        => $statusNiaga
+                ],
+            ]
+        ];
+        $response = [
+                'status'    => "success",
+                'message'   => $message,
+                'data'      => $dataContent
+            ];
+            return response()->json($response, 200);
+    }
 }
